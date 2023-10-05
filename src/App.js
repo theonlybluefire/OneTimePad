0f9206979495da305385238ce00d92f8af680d10
Enrico <white.privat@gmail.com>
2023-10-05 21:15:23 +0200

    done the random display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 import generateKey from './components/randomGenerator';
+//others
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import { useState } from 'react';
+
+
 function App() {
-  const test = 'halllloa'
-  let key = generateKey(test)
-  console.log(key)
+const [keyDisplay, setKeyDisplay] = useState('ichts')
+const [text, setText] = useState('')
+//handle Input
+const handleChange = (event) => {
+  setText(event.target.value)
+  let key = generateKey(event.target.value);
+  const keyElements = key.map((keyValue, index) => (
+    <span key={index}>{keyValue}</span>
+  ));
+  setKeyDisplay(keyElements)
+}
   return (
     <div>
-
+      <div class="text-center">
+        {keyDisplay}
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">input</label>
+        <input class="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleChange}></input>
+      </div>
     </div>
   );
 }
