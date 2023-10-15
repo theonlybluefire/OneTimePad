435e62f8093726ddadfc8a2889812ac60e697483
Enrico <white.privat@gmail.com>
2023-10-15 21:28:39 +0200

    forgot

--- src/components/otpAlgo.js
+++ src/components/otpAlgo.js
@@ -1,3 +1,23 @@
 const input = (phrase, key) => {
-    
+    var lengthPhrase = phrase.length
+    var numbersPhrase = []
+    var lenghtKey = key.length
+    var numbersKey = []
+    var returnArray = []
+    function letterToNumber(letter) {
+        var asciiValue = letter.toUpperCase().charCodeAt(0);
+        return asciiValue - 64;
+      }
+    for(let i=0;i<lengthPhrase;i++) {
+        let number = letterToNumber(phrase[i]);
+        numbersPhrase.push(number)
+    }
+    for(let i=0;i<lenghtKey;i++) {
+        let number = letterToNumber(key[i]);
+        numbersKey.push(number)
+    }
+    //addition
+    for(let i=0;i<lengthPhrase;i++) {
+        returnArray.push(numbersKey[i]+numbersPhrase[i])
+    }
 }
