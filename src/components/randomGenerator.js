052f1bcdc91eade5d069d4edd517cef88c611b60
Enrico <white.privat@gmail.com>
2023-11-11 20:59:39 +0100

    done some things to the algo
    now working but some errors with the random generator

--- src/components/randomGenerator.js
+++ src/components/randomGenerator.js
@@ -1,6 +1,6 @@
 const generateKey = (phrase) => {
-    var lengthPhrase = phrase.length-1;
-    console.info('Generating Key ....')
+    var lengthPhrase = phrase.length;
+    console.info('Generating Key ....');
     var key = [];
     const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     for(let i=0; i<lengthPhrase; i++) {
